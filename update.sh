5ad5507d859445c6642587ebf390fab006cb691c
Andrew Mao <mizzao@gmail.com>
2014-10-31 17:14:46 -0400

    Allow update script to take a tag

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+# USAGE: ./update.sh vX.Y.Z
+# where X.Y.Z is the tag of the BS3 repo
 
 if [ ! -d "git-repo" ]; then
   git clone https://github.com/twbs/bootstrap.git git-repo
@@ -7,7 +9,8 @@ fi
 # rm -rf bootstrap-3/css/bootstrap-glyphicons.css
 cd "git-repo/"
 git checkout -- .
-git pull
+git fetch
+git checkout $@
 npm install
 grunt dist
 cp -r dist/* ../bootstrap-3/
