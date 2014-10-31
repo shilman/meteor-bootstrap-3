f34117adaf8e59c4607ea860da34b6f852b56fde
Andrew Mao <mizzao@gmail.com>
2014-10-31 17:16:42 -0400

    3.3.0 build

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "mizzao:bootstrap-3",
   summary: "HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.",
-  version: "3.2.0_1",
+  version: "3.3.0",
   git: "https://github.com/mizzao/meteor-bootstrap-3.git"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom("0.9.3.1");
+  api.versionsFrom("1.0");
 
   api.use('jquery');
 
@@ -24,5 +24,8 @@ Package.onUse(function (api) {
   // XXX this makes the paths to the icon sets absolute. it needs
   // to be included _after_ the standard bootstrap css so
   // that its styles take precedence.
+
+  // If this file is not added, browsers using the icon fonts will not be able
+  // to load them if the page is initially accessed at an url other than /.
   api.addFiles(path.join('bootstrap-override.css'), 'client');
 });
